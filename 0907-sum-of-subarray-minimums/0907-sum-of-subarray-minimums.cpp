990169d0c891ab043f4db379065a90e9c157014e
Niharika <niharikapenugonda17@gmail.com>
2026-06-24 08:00:19 +0530

    Time: 68 ms (31.61%), Space: 193.4 MB (30.34%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    void findNSE(vector<int>& nse, int n, vector<int> arr) {
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        const int mod = 1e9 + 7;
+
+        vector<int> nse(n), pse(n);
         stack<int> st;
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && arr[st.top()] >= arr[i]) {
+
+        // NSE: next smaller element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
                 st.pop();
             }
             nse[i] = st.empty() ? n : st.top();
             st.push(i);
         }
-    }
-    void findPSE(vector<int>& pse, int n, vector<int> arr) {
-        stack<int> st;
-        for(int i=0; i<n; i++) {
-            while(!st.empty() && arr[st.top()] > arr[i]) {
+
+        while (!st.empty()) st.pop();
+
+        // PSE: previous smaller element
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
                 st.pop();
             }
             pse[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
-    }
 
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> nse(n);
-        vector<int> pse(n);
-        findNSE(nse, n, arr);
-        findPSE(pse, n, arr);
         long long sum = 0;
-        const int mod = 1e9 + 7;
 
         for (int i = 0; i < n; i++) {
-            long long left = nse[i]-i;
-            long long right = i-pse[i];
+            long long left = i - pse[i];
+            long long right = nse[i] - i;
+
             sum = (sum + left * right % mod * arr[i]) % mod;
         }
 
