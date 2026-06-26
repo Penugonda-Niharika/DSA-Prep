d554f1592683875e43bb902b9007eddf7ca7cc95
Niharika <niharikapenugonda17@gmail.com>
2026-06-26 19:23:17 +0530

    Time: 20 ms (64.1%), Space: 81.3 MB (71.94%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    void nse(vector<int> arr, vector<int>& nse) {
+    int largestRectangleArea(vector<int>& arr) {
+        int maxArea = 0;
         int n = arr.size();
         stack<int> st;
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            nse[i] = st.empty() ? n : st.top();
-            st.push(i); 
-        }
-    }
-    void pse(vector<int> arr, vector<int>& pse) {
-        int n = arr.size();
-        stack<int> st;
-        for(int i=0; i<n; i++) {
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            pse[i] = st.empty() ? -1 : st.top();
-            st.push(i); 
-        }
-    }
-
-    int largestRectangleArea(vector<int>& h) {
-        int ma = 0;
-        int n = h.size();
-        vector<int> nsee(n);
-        vector<int> psee(n);
-        nse(h, nsee);
-        pse(h, psee);
         for (int i = 0; i < n; i++) {
-            ma = max(ma, h[i]*(nsee[i]-psee[i]-1));
+            while(!st.empty() && arr[st.top()]>arr[i]) {
+                int el = st.top();
+                st.pop();
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+                maxArea = max(maxArea, arr[el]*(nse-pse-1));
+            }
+            st.push(i);
         }
-        return ma;
+        while(!st.empty()) {
+            int el = st.top();
+            st.pop();
+            int nse = n;
+            int pse = st.empty() ? -1 : st.top();
+            maxArea = max(maxArea, arr[el]*(nse-pse-1));
+        }
+        return maxArea;
     }
 };
