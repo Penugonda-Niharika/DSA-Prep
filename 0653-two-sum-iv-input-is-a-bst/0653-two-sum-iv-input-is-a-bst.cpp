5944e00b583beee1c9495c46e2b66c0faa5f6c12
Niharika <niharikapenugonda17@gmail.com>
2026-07-10 06:16:47 +0530

    Time: 0 ms (100%), Space: 37.3 MB (46.92%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,33 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+    stack<TreeNode*>st;
+    bool rev;
+    void pushAll(TreeNode* node) {
+        while(node) {
+            st.push(node);
+            if(rev) {
+                node = node->right;
+            } else {
+                node = node->left;
+            }
+        }
+    }
+public:
+    BSTIterator(TreeNode* root, bool isRev) {
+        rev = isRev;
+        pushAll(root);
+    }
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        if(rev) {
+            pushAll(node->left);
+        } else {
+            pushAll(node->right);
+        }
+        return node->val;
+    }
+};
+
 class Solution {
 public:
-    bool twoPointer(int k, vector<int> ans) {
-        int st = 0, end = ans.size()-1;
-        while(st < end) {
-            int sum = ans[st] + ans[end];
-            if(sum == k) {
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+        int i = l.next();
+        int j = r.next();
+        while(i < j) {
+            int s = i+j;
+            if(s==k) {
                 return true;
-            } else if(sum > k) {
-                end--;
+            } else if(s>k) {
+                j = r.next();
             } else {
-                st++;
+                i = l.next();
             }
         }
         return false;
-    }
-
-    void inorder(TreeNode* root, int k, vector<int>& ans) {
-        if(root == NULL) return;
-        inorder(root->left, k, ans);
-        ans.push_back(root->val);
-        inorder(root->right, k, ans);
-    }
-
-    bool findTarget(TreeNode* root, int k) {
-        vector<int> ans;
-        inorder(root, k, ans);
-        return twoPointer(k, ans);
     }
 };
