a311d990777a1e2c5f466009aa1bc0db0196c97c
Niharika <niharikapenugonda17@gmail.com>
2026-08-24 07:50:12 +0530

    Time: 1 ms (58.42%), Space: 48.6 MB (27.9%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,21 +1,25 @@
 class Solution {
 
     public int trap(int[] h) {
-
-        int lmax = 0;
+        int n = h.length;
+        int lmax[] = new int[n];
+        int rmax[] = new int[n];
         int ans = 0;
+
+        lmax[0] = h[0];
+        rmax[n-1] = h[n-1];
+
+        for(int i=1; i<n; i++) {
+            lmax[i] = Math.max(lmax[i-1], h[i]);
+        }
+
+        for(int i=n-2; i>=0; i--) {
+            rmax[i] = Math.max(rmax[i+1], h[i]);
+        }
 
         for(int i = 0; i < h.length; i++) {
 
-            int rmax = 0;
-
-            lmax = Math.max(lmax, h[i]);
-
-            for(int j = i + 1; j < h.length; j++) {
-                rmax = Math.max(rmax, h[j]);
-            }
-
-            ans += Math.max(0, Math.min(lmax, rmax) - h[i]);
+            ans += Math.max(0, Math.min(lmax[i], rmax[i]) - h[i]);
         }
 
         return ans;
