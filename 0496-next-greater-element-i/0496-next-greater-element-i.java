87e1f03e92d11de94e89ff2e25490594f7c72681
Niharika <niharikapenugonda17@gmail.com>
2026-08-24 06:06:35 +0530

    Time: 4 ms (74.22%), Space: 45.3 MB (36.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,30 +1,28 @@
 class Solution {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
         int ans[] = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-
-            for (int j = 0; j < nums2.length; j++) {
-
-                if (nums1[i] == nums2[j]) {
-
-                    ans[i] = -1;
-
-                    for (int k = j + 1; k < nums2.length; k++) {
-
-                        if (nums2[k] > nums2[j]) {
-                            ans[i] = nums2[k];
-                            break;
-                        }
-                    }
-
-                    break;
+        for(int i=nums2.length-1; i>=0; i--) {
+            if(st.empty()) {
+                st.push(nums2[i]);
+                mp.put(nums2[i], -1);
+            } else {
+                while(!st.empty() && st.peek()<=nums2[i]) {
+                    st.pop();
                 }
+                if(st.empty()) {
+                   mp.put(nums2[i], -1);
+                } else {
+                    mp.put(nums2[i], st.peek());
+                }
+                st.push(nums2[i]);
             }
         }
-
+        for(int i=0; i<nums1.length; i++) {
+            ans[i] = mp.get(nums1[i]);
+        }
         return ans;
     }
 }
