47f25baa4456fa5d03f1dc8b172b6fccefb48976
Niharika <niharikapenugonda17@gmail.com>
2026-09-05 07:29:50 +0530

    Time: 1 ms (99.48%), Space: 47.6 MB (24.4%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,39 +1,51 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
         int n = grid.length;
-        boolean found = false;
-        int cnt = 0;
         int arr[] = new int[2];
+        int xor = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                int el = grid[i][j];
-                cnt = 0;
-                for(int k=0; k<n; k++) {
-                    for(int l=0; l<n; l++) {
-                        if(grid[k][l] == el) {
-                            cnt++;
-                        }
-                    }
-                }
-                if(cnt==2) {
-                    arr[0] = el;
-                    break;
-                } 
-            }
-            if (arr[0] != 0) {
-                 break;
+                xor ^= grid[i][j];
+               
             }
         }
-        for(int k=1; k<=n*n; k++) {
-            found = false;
-            for(int i=0; i<n; i++) {
-                for(int j=0; j<n; j++) {
-                    if(grid[i][j] == k) found = true;
+
+        for(int i=1; i<=n*n; i++) {
+            xor ^= i;
+        }
+        int rbit = xor & -xor;
+        int g1 = 0;
+        int g2 = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if((grid[i][j] & rbit)!=0) {
+                    g1 ^= grid[i][j];
+                } else {
+                    g2 ^= grid[i][j];
                 }
             }
-            if(!found) {
-                arr[1] = k;
-                break;
+        }
+
+        for(int i=1; i<=n*n; i++) {
+            if((i & rbit)!=0) {
+                g1 ^= i;
+            } else {
+                g2 ^= i;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == g1) {
+                    arr[0] = g1;
+                    arr[1] = g2;
+                    break;
+                } 
+                if(grid[i][j] == g2) {
+                    arr[0] = g2;
+                    arr[1] = g1;
+                    break;
+                } 
             }
         }
         return arr;
