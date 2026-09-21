523113393dcb68f3c3bf8fc5ca2269c2ca12d45f
Niharika <niharikapenugonda17@gmail.com>
2026-09-21 07:01:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (70.75%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null && q != null || q==null && p != null) return false;
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.add(p);
+        q2.add(q);
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode n1 = q1.poll();
+            TreeNode n2 = q2.poll();
+            if(n1 == null && n2 != null || n2 == null && n1 != null || n1.val != n2.val) {
+                return false;
+            }
+            if(n1.left != null && n2.left == null || n1.left == null && n2.left != null) return false;
+            if(n1.right != null && n2.right == null || n1.right == null && n2.right != null) return false;
+            if(n1.left != null && n2.left != null) {
+                q1.add(n1.left);
+                q2.add(n2.left);
+            }
+            if(n1.right != null && n2.right != null) {
+                q1.add(n1.right);
+                q2.add(n2.right);
+            }
+        }
+        return true;
+    }
+}
